17cf21ff0b93aacf44d3291a34c388766d2eae5a
Muhamad Raihan <muhamadraihan1611@gmail.com>
2026-07-01 22:20:53 +0700

    add task_details file and modified home_screen

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:todoapp/data/data.dart';
+import 'package:todoapp/utils/utils.dart';
 
 class TaskTile extends StatelessWidget {
-  const TaskTile({super.key, required this.task});
+  const TaskTile({super.key, required this.task, this.onCompleted});
   final Task task;
+  final Function(bool?)? onCompleted;
 
   @override
   Widget build(BuildContext context) {
+    final style = context.textTheme;
+    final double iconOpacity = task.isCompleted ? 0.3 : 0.5;
+    final backgroundOpacity = task.isCompleted ? 0.1 : 0.3;
+    final textDecoration = task.isCompleted
+        ? TextDecoration.lineThrough
+        : TextDecoration.none;
+    final fontWeight = task.isCompleted ? FontWeight.normal : FontWeight.bold;
+
     return Padding(
       padding: const EdgeInsets.only(left: 16, top: 10, bottom: 10),
       child: Row(
@@ -16,21 +26,42 @@ class TaskTile extends StatelessWidget {
             padding: const EdgeInsets.all(9.0),
             decoration: BoxDecoration(
               shape: BoxShape.circle,
-              color: task.category.color.withValues(alpha: 0.2),
-              border: Border.all(width: 2, color: task.category.color),
+              color: task.category.color.withValues(alpha: backgroundOpacity),
+              border: Border.all(
+                width: 2,
+                color: task.category.color.withValues(alpha: iconOpacity),
+              ),
             ),
             child: Center(
-              child: Icon(task.category.icon, color: task.category.color),
+              child: Icon(
+                task.category.icon,
+                color: task.category.color.withValues(alpha: iconOpacity),
+              ),
             ),
           ),
           const Gap(16),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [Text(task.title), Text(task.time)],
+              children: [
+                Text(
+                  task.title,
+                  style: style.titleMedium?.copyWith(
+                    decoration: textDecoration,
+                    fontSize: 20,
+                    fontWeight: fontWeight,
+                  ),
+                ),
+                Text(
+                  task.time,
+                  style: style.titleMedium?.copyWith(
+                    decoration: textDecoration,
+                  ),
+                ),
+              ],
             ),
           ),
-          Checkbox(value: task.isCompleted, onChanged: (value) {}),
+          Checkbox(value: task.isCompleted, onChanged: onCompleted),
         ],
       ),
     );
